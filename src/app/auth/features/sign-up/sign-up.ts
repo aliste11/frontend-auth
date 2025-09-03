45cdb0c1ed516369d359f45ffb48a58ca4ba92cd
Igacio Aliste Toledo <aliste11toledo@gmail.com>
2025-09-03 15:36:38 -0400

    feat: Mejorar el formulario de registro con validaciones y manejo de estado

--- src/app/auth/features/sign-up/sign-up.ts
+++ src/app/auth/features/sign-up/sign-up.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
+import { AuthService } from '../../data-access/auth.service';
 
+interface loginForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './sign-up.html',
   styles: ``
 })
 export default class SignUp {
+  private authService = inject(AuthService)
+  form;
 
-}
+  constructor(private _formBuilder: FormBuilder) {
+    this.form = this._formBuilder.group<loginForm>({
+      email: this._formBuilder.nonNullable.control('', Validators.required),
+      password: this._formBuilder.nonNullable.control('', Validators.required),
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) return;
+
+    const { email, password } = this.form.getRawValue();
+    this.authService.signUp(email, password).subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error)
+    })
+  }
+
+  }
